f3d44d4e45e6be9f48c3ed335cb30a2926966ac5
HackerZZZZZZ <hackerifyouwant@gmail.com>
2020-05-03 10:20:27 +0800

    new

--- UVA/245.cpp
+++ UVA/245.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<cctype>
-
-#define ll long long
-#define endl '\n'
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
 
 using namespace std;
 
+vector<string> word_list;
 int main(){
-    char c;
-    string s;
-    vector<string> v;
-    while(getline(cin, s) && s[0] != '0'){
-        for(int i = 0; i < s.size(); i++){
-            string test;
-            if(isalpha(s[i])){
-                cout << s[i];
-                test += s[i];
-            }else if(isdigit(s[i])){
-                cout << int(s[i]- '0');
-                //cout << v[int(s[i]- '0')];
-            }else{
-                if(s[i] == ' '){
-                    v.insert(v.begin(), test);
-                    cout << test << " ";
-                    test = "";
-                }else{
-                    cout << s[i];
-                }
-            }
-        }
-        cout << endl;
-    }
+	string buf;
+	while (getline(cin, buf) && buf[0] != '0') {
+		int value = 0, state = 0; // 2:number, 1:letter, 0:others
+		string s;
+		for (int i = 0; buf[i]; ++ i) { 
+			if (isalpha(buf[i])) {
+				s = "";
+				while (isalpha(buf[i]))
+					s.insert(s.end(), buf[i ++]);
+				word_list.push_back(s);
+				i --;
+				cout << s;
+			} else if (isdigit(buf[i])) {
+				value = 0;
+				while (isdigit(buf[i]))
+					value = value * 10 + buf[i ++] - '0';
+				s = word_list[word_list.size() - value];
+				word_list.erase(word_list.end() - value);
+	        	word_list.push_back(s);
+	        	i --;
+				cout << s;
+			} else 
+                cout << buf[i];
+		}
+		cout << "" << endl;
+	}
     return 0;
 }
